78b89646f70410b50337a025bbc97959982e732b
yiyuezhuo <616271835@163.com>
2016-04-13 19:27:27 +0800

    add CLI and python3 support

--- main.py
+++ main.py
@@ -16,16 +16,17 @@ num:300
 import requests
 import json
 import pandas as pd
+import os
 
 def get(cityId='510100',scope='city',date='3',dimension='satisfy',num=1000):
     url='http://v.kuaidadi.com/point'
     params={'cityId':cityId,'scope':scope,'date':date,'dimension':dimension,'num':num}
     res=requests.get(url,params=params)
-    return json.loads(res.content)
+    return json.loads(res.content.decode())
     
 class Downloader(object):
-    def __init__(self):
-        self.cityId_list=['510100']
+    def __init__(self,cityId_list=None):
+        self.cityId_list=cityId_list if cityId_list!=None else ['510100']
         self.scope_list=['city']
         self.date_list=[str(i) for i in range(7)]
         self.dimension_list=['distribute','satisfy','demand','response','money']
@@ -48,7 +49,7 @@ class Downloader(object):
             params=dict(zip(pkey,key))
             self.data[key]=get(**params)
             if verbose:
-                print 'clear',key
+                print('clear',key)
                 
 def to_csv(key,json_d,prefix='data/'):
     data=json_d['result']['data']
@@ -68,6 +69,30 @@ def to_csv(key,json_d,prefix='data/'):
 def to_csv_all(datas,path='data/'):
     for key,json_d in datas.items():
         to_csv(key,json_d,prefix=path)
+        
+def run(city,path='data'):
+    if not os.path.isdir(path):
+        print('create dir path',path)
+        os.mkdir(path)
+    downloader=Downloader([city])
+    downloader.download()
+    to_csv_all(downloader.data,path=path+'/')
+
+        
+def CLI():
+    import argparse
+    parser = argparse.ArgumentParser(usage=u'python main.py 510100',
+                                     description=u"苍穹平台数据抓取器")
+    parser.add_argument('city',help=u'城市序号，成都是510100,其他ID参见cityId.json文件')
+    parser.add_argument('--dir',default='data',help=u'保存路径,默认为data')
+    args=parser.parse_args()
+    run(args.city,args.dir)
+
+        
+if __name__=='__main__':
+    import sys
+    if len(sys.argv)>1:
+        CLI()
 
 '''
 downloader=Downloader()
